1a4b9ca81c9ef6a9e8a6a80109ced3ca145d0e95
allamvinay <121039243+allamvinay@users.noreply.github.com>
2023-10-04 14:15:28 +0530

    commiting c language code

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	char str[100];
+	int i;
+	printf("enter the value:");
+	scanf("%s%d",str,&i);
+	printf("you have entered:%s:::::%d",str,i);
+	return 0;
+	
+	
+}
+
